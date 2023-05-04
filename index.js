d6d1eb02621330d9e51e68647308e7973d08a103
Peter <113168584+prawstho@users.noreply.github.com>
2023-05-04 10:12:00 -0230

    lecture over

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
+
 const server = http.createServer((req, res) => {
+    console.log(req);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Keyin World');
+    res.end('Hello Keyin World on a foggy day.');
 });
-console.log("It is sunny today.")
+
+console.log("It is foggy today.")
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
